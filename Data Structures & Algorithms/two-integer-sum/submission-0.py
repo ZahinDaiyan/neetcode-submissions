b3b5b027f55a95caf5460c187301db7de594c70e
Siam <71772943+ZahinDaiyan@users.noreply.github.com>
2026-09-26 00:11:18 +0600

    Update: two-integer-sum - submission-0

--- Data Structures & Algorithms/two-integer-sum/submission-0.py
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -1,13 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # map
-        hmap = {}
-        # Now ki korbo ? we see if we have the diff in the map or not
-        for i , n in enumerate(nums):
-            diff = target - n
-            if diff in hmap:
-                return [hmap[diff] , i]   
-            else:         
-                hmap[n] = i
 
-       
+        # ["key":"value"]
+        mapp = {} 
+
+        for i in range (len(nums)):
+            complement = target - nums[i]
+            if complement in mapp:
+                return [mapp[complement], i]
+            mapp[nums[i]] = i
